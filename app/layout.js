144500a83b6dce03c51262110c32653ca868a634
Divine Patrick <patrickdivine70@gmail.com>
2025-12-06 15:16:53 +0100

    added authentication with clerk

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -13,6 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider> 
     <html lang="en">
       <body
         className={`${inter.className} antialiased`}
@@ -20,5 +22,6 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    </ ClerkProvider>
   );
 }
